d95cf108bea87bb282f7dee269212247e901adf0
singhcse <singhcse01@gmail.com>
2018-11-06 20:22:01 +0530

    first commit in DSALGO

---
+++ src/main/java/com/example/dsalgo/DsalgoApplication.java
@@ -0,0 +1,12 @@
+package com.example.dsalgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsalgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsalgoApplication.class, args);
+	}
+}
