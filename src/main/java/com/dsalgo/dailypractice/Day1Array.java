70e4aaf81721f6fbb57aee9597a4437fc09599fd
singhcse <singhcse01@gmail.com>
2018-11-06 20:53:44 +0530

    ArrayDay1

--- src/main/java/com/dsalgo/dailypractice/Day1Array.java
+++ src/main/java/com/dsalgo/dailypractice/Day1Array.java
@@ -2,4 +2,25 @@ package com.dsalgo.dailypractice;
 
 public class Day1Array {
 
+	//1. Find the missing number in an array of 1 to 100
+	
+	public static int missingNum(int[] arr) {
+		int sum = 0;
+		for(int i=0;i<99;i++) {
+			sum+= arr[i];
+		}
+		return (100*(100+1)/2)-sum;
+	}
+	
+	public static void main(String []arg) {
+		int[] missingNum = new int[99];
+		int number = 100;
+		for(int j=0;j<99;j++) {
+			missingNum[j] = number--;
+		}
+		
+		System.out.println(missingNum(missingNum));
+	}
+	
+	
 }
